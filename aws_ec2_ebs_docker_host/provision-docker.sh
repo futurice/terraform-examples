c3da779b2399851caf85453dc3027e1975131cc6
Jarno Rantanen <jarno@jrw.fi>
2019-03-21 15:45:48 +0200

    Remove docker-compose from aws_ec2_ebs_docker_host.

--- aws_ec2_ebs_docker_host/provision-docker.sh
+++ aws_ec2_ebs_docker_host/provision-docker.sh
@@ -5,10 +5,6 @@ curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 rm get-docker.sh
 
-# https://docs.docker.com/compose/install/
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 # Allow using docker without sudo
 sudo usermod -aG docker $(whoami)
 
