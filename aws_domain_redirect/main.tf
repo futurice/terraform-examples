1c0136b4682c480638a0c635f161cd91cd76e00b
Jarno Rantanen <jarno@jrw.fi>
2019-04-04 19:07:03 +0300

    Update vars to match new version of aws_static_site.

--- aws_domain_redirect/main.tf
+++ aws_domain_redirect/main.tf
@@ -3,12 +3,12 @@ module "aws_static_site" {
   # Check for updates: https://github.com/futurice/terraform-utils/compare/v9.1...master
   source = "git::ssh://git@github.com/futurice/terraform-utils.git//aws_static_site?ref=v9.1"
 
-  site_domain                 = "${var.redirect_domain}"
-  name_prefix                 = "${var.name_prefix}"
-  comment_prefix = "${var.comment_prefix}"
-  bucket_override_name        = "-"                             # providing this ensures an S3 bucket isn't unnecessarily created, even if this isn't a valid bucket name
-  price_class                 = "${var.redirect_price_class}"
-  lambda_logging_enabled      = "${var.lambda_logging_enabled}"
+  site_domain            = "${var.redirect_domain}"
+  name_prefix            = "${var.name_prefix}"
+  comment_prefix         = "${var.comment_prefix}"
+  bucket_override_name   = "-"                             # providing this ensures an S3 bucket isn't unnecessarily created, even if this isn't a valid bucket name
+  price_class            = "${var.redirect_price_class}"
+  lambda_logging_enabled = "${var.lambda_logging_enabled}"
 
   add_response_headers = {
     "Strict-Transport-Security" = "${var.redirect_with_hsts ? "max-age=31557600; preload" : ""}"
