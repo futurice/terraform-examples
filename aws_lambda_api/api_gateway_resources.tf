68577e262d8ca26fa4f75fc2b2fdca84c64813e0
Jarno Rantanen <jarno@jrw.fi>
2019-04-11 16:04:29 +0300

    Split the API Gateway config into 2 files.

--- aws_lambda_api/api_gateway_resources.tf
+++ aws_lambda_api/api_gateway_resources.tf
@@ -1,10 +1,3 @@
-# Configure an API Gateway instance:
-
-resource "aws_api_gateway_rest_api" "this" {
-  name        = "${local.prefix_with_domain}"
-  description = "${var.comment_prefix}${var.api_domain}"
-}
-
 # Add root resource to the API (it it needs to be included separately from the "proxy" resource defined below), which forwards to our Lambda:
 
 resource "aws_api_gateway_method" "proxy_root" {
@@ -122,51 +115,4 @@ resource "aws_api_gateway_method_response" "proxy_cors" {
     "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Origin"  = true
   }
-}
-
-resource "aws_api_gateway_deployment" "this" {
-  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-
-  depends_on = [
-    "aws_api_gateway_integration.proxy_root",
-    "aws_api_gateway_integration.proxy_other",
-    "aws_api_gateway_integration.proxy_cors",
-  ]
-}
-
-resource "aws_api_gateway_stage" "this" {
-  stage_name    = "${var.stage_name}"
-  description   = "${var.comment_prefix}${var.api_domain}"
-  rest_api_id   = "${aws_api_gateway_rest_api.this.id}"
-  deployment_id = "${aws_api_gateway_deployment.this.id}"
-  tags          = "${var.tags}"
-}
-
-resource "aws_api_gateway_method_settings" "this" {
-  rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-  stage_name  = "${aws_api_gateway_stage.this.stage_name}"
-  method_path = "*/*"
-
-  settings {
-    metrics_enabled        = "${var.api_gateway_cloudwatch_metrics}"
-    logging_level          = "${var.api_gateway_logging_level}"
-    data_trace_enabled     = "${var.api_gateway_logging_level == "OFF" ? false : true}"
-    throttling_rate_limit  = "${var.throttling_rate_limit}"
-    throttling_burst_limit = "${var.throttling_burst_limit}"
-  }
-}
-
-resource "aws_api_gateway_domain_name" "this" {
-  domain_name              = "${var.api_domain}"
-  regional_certificate_arn = "${aws_acm_certificate_validation.this.certificate_arn}"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-resource "aws_api_gateway_base_path_mapping" "this" {
-  api_id      = "${aws_api_gateway_rest_api.this.id}"
-  stage_name  = "${aws_api_gateway_stage.this.stage_name}"
-  domain_name = "${aws_api_gateway_domain_name.this.domain_name}"
 }
