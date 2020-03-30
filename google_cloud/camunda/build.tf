e47a477e77fcd18bdea94cacd4d61af9269be9ac
Tom Larkworthy <tom.larkworthy.external@zalando.de>
2020-03-30 20:17:15 +0200

    Customize bpm-platform.xml configuration before deployment

--- google_cloud/camunda/build.tf
+++ google_cloud/camunda/build.tf
@@ -16,13 +16,33 @@ resource "local_file" "dockerfile" {
   filename = "${path.module}/.build/Dockerfile"
 }
 
+# Hydrate bpm-platform config into .build directory
+resource "local_file" "bpm-platform" {
+  content = templatefile("${path.module}/config/bpm-platform.xml.template", {
+    maxJobsPerAcquisition = null
+    lockTimeInMillis = null
+    waitTimeInMillis = 1
+    maxWait = null
+    history = "none"
+    databaseSchemaUpdate = null # default
+    authorizationEnabled = null # default
+    jobExecutorDeploymentAware = "false"
+    historyCleanupBatchWindowStartTime = null # default
+  })
+  filename = "${path.module}/.build/bpm-platform.xml"
+}
+
 # Build a customized image of Camunda to include the cloud sql postgres socket factory library
 # Required to connect to Cloud SQL
 # Built using Cloud Build, image stored in GCR
 resource "null_resource" "camunda_cloudsql_image" {
   triggers = {
-    # Rebuild if we change the base image or the local docker
-    image = "eu.gcr.io/${local.project}/camunda_cloudsql:${local.config.base_image_tag}_${sha1(local_file.dockerfile.content)}"
+    # Rebuild if we change the base image, dockerfile, or bpm-platform config
+    image = "eu.gcr.io/${local.project}/camunda_cloudsql:${local.config.base_image_tag}_${
+      sha1(
+        "${sha1(local_file.dockerfile.content)}${sha1(local_file.bpm-platform.content)}"
+      )  
+    }"
   }
   provisioner "local-exec" {
     command = <<-EOT
