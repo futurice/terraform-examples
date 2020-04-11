e6d49e18d3dd75e368e5db6359f4849b4201569b
Tom Larkworthy <tom.larkworthy.external@zalando.de>
2020-04-12 01:11:50 +0200

    Better documentation in main.tf

--- google_cloud/minecraft/main.tf
+++ google_cloud/minecraft/main.tf
@@ -1,3 +1,21 @@
+/*
+A safe Minecraft server that won't break the bank. Game data is preserved across sessions.
+Server is hosted on a permenant IP address. You need to start the VM each session, but it
+will shutdown within 24 hours if you forget to turn it off.
+Process is run in a sandboxed VM, so any server exploits cannot do any serious damage.
+
+Features
+- Runs [itzg/minecraft-server](https://hub.docker.com/r/itzg/minecraft-server/) Docker image
+- Preemtible VM (cheapest), shuts down automatically within 24h if you forget to stop the VM
+- Reserves a stable public IP, so the minecraft clients do not need to be reconfigured
+- Reserves the disk, so game data is remembered across sessions
+- Restricted service account, VM has no ability to consume GCP resources beyond its instance and disk
+- 2$ per month
+  - Reserved IP address costs: $1.46 per month
+  - Reserved 10Gb disk costs: $0.40
+  - VM cost: $0.01 per hour, max session cost $0.24
+*/
+
 # We require a project to be provided upfront
 # Create a project at https://cloud.google.com/
 # Make note of the project ID
