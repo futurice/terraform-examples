4163130cdd950043726d41db67a331fef84b10b3
Tom Larkworthy <tom.larkworthy.external@zalando.de>
2020-04-12 00:59:58 +0200

    Minecraft server recipe

---
+++ google_cloud/minecraft/main.tf
@@ -0,0 +1,98 @@
+# We require a project to be profided upfront
+# Create a project at https://cloud.google.com/
+# Make note of the project ID
+# We need a storage bucket created upfront too to store the terraform state
+terraform {
+  backend "gcs" {
+    prefix = "minecraft/state"
+    bucket = "terraform-larkworthy"
+  }
+}
+
+# You need to fill these locals out with the project, region and zone
+# Then to boot it up, run:-
+#   gcloud auth application-default login
+#   terraform init
+#   terraform apply
+locals {
+  # The Google Cloud Project ID that will host and pay for your Minecraft server
+  project = "larkworthy-tester"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+provider "google" {
+  project = local.project
+  region  = local.region
+}
+
+# Create service account to run service with no permissions
+resource "google_service_account" "minecraft" {
+  account_id   = "minecraft"
+  display_name = "minecraft"
+}
+
+# Permenant Minecraft disk, stays around when VM is off
+resource "google_compute_disk" "minecraft" {
+  name  = "minecraft"
+  type  = "pd-standard"
+  zone  = local.zone
+  image = "cos-cloud/cos-stable"
+}
+
+# Permenant IP address, stays the same
+resource "google_compute_address" "minecraft" {
+  name   = "minecraft-ip"
+  region = local.region
+}
+
+# VM to run Minecraft, we use preemptable which will shutdown within 24 hours
+resource "google_compute_instance" "minecraft" {
+  name         = "minecraft"
+  machine_type = "n1-standard-1"
+  zone         = local.zone
+  tags         = ["minecraft"]
+
+  # Run itzg/minecraft-server docker image on startup
+  # The instructions of https://hub.docker.com/r/itzg/minecraft-server/ are applicable
+  # For instance, Ssh into the instance and you can run
+  #  docker logs mc
+  #  docker exec -i mc rcon-cli
+  # Once in rcon-cli you can "op <player_id>" to make someone an operator (admin)
+  # Use 'sudo journalctl -u google-startup-scripts.service' to retrieve the startup script output
+  metadata_startup_script = "docker run -d -p 25565:25565 -e EULA=TRUE -v /var/minecraft:/data --name mc --rm=true itzg/minecraft-server:latest;"
+
+  boot_disk {
+    auto_delete = false # Keep disk after shutdown (game data)
+    source      = google_compute_disk.minecraft.self_link
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.minecraft.address
+    }
+  }
+
+  service_account {
+    email  = google_service_account.minecraft.email
+    scopes = ["userinfo-email"]
+  }
+
+  scheduling {
+    preemptible = true # Closes within 24 hours (sometimes sooner)
+    automatic_restart = false
+  }
+}
+
+# Open the firewall
+resource "google_compute_firewall" "minecraft" {
+  name    = "minecraft"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["25565"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["minecraft"]
+}
