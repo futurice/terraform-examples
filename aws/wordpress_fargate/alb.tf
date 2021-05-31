10d825763ecedc84cb0a5985d0f65fe0c982fef8
Jacob Emmert-Aronson <jacob@mlaronson.com>
2021-05-31 16:35:55 -0700

    Modernize TLS ciphersuites

--- aws/wordpress_fargate/alb.tf
+++ aws/wordpress_fargate/alb.tf
@@ -49,6 +49,7 @@ module "alb" {
     {
       "certificate_arn" = module.acm_alb.this_acm_certificate_arn
       "port"            = 443
+      "ssl_policy"      = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
     },
   ]
 
