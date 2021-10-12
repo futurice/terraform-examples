6419f2805271453108012ec391995b4e16acc7eb
jyoti1998-gehlot <74006741+jyoti1998-gehlot@users.noreply.github.com>
2021-10-12 09:46:35 +0530

    Update target_group_attach.tf

--- aws/aws_application_load_balancer/target_group_attach.tf
+++ aws/aws_application_load_balancer/target_group_attach.tf
@@ -2,7 +2,7 @@ resource "aws_lb_target_group" "target-lb" {
   name     = "lb-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "enter-you-vpc-id"
+  vpc_id   = "enter_vpc_id"
 }
 resource "aws_lb_target_group_attachment" "test1" {
   target_group_arn = aws_lb_target_group.target-lb.arn
